444f83e8223b8fba2053150e732720d7b3d6aec2
karoldavid <k.zysk@zoho.com>
2018-02-04 20:38:48 +0100

    feat: render the header columns with the labels from the CART_ITEM_FIELDS

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -10,43 +10,25 @@ import {
 } from "material-ui/Table";
 
 class ShoppingCart extends Component {
+	renderHeaderColumns = data => {
+		return data.map((item, index) => {
+			return <TableHeaderColumn key={index}>{item.label}</TableHeaderColumn>;
+		});
+	};
 
 	render() {
-		console.log(this.props.data);
+		const { data } = this.props;
+
 		return (
 			<Table>
 				<TableHeader>
-					<TableRow>
-						<TableHeaderColumn>ID</TableHeaderColumn>
-						<TableHeaderColumn>Name</TableHeaderColumn>
-						<TableHeaderColumn>Status</TableHeaderColumn>
-					</TableRow>
+					<TableRow>{this.renderHeaderColumns(data)}</TableRow>
 				</TableHeader>
 				<TableBody>
 					<TableRow>
-						<TableRowColumn>1</TableRowColumn>
-						<TableRowColumn>John Smith</TableRowColumn>
+						<TableRowColumn>qwertz</TableRowColumn>
+						<TableRowColumn>qwertz</TableRowColumn>
 						<TableRowColumn>Employed</TableRowColumn>
-					</TableRow>
-					<TableRow>
-						<TableRowColumn>2</TableRowColumn>
-						<TableRowColumn>Randal White</TableRowColumn>
-						<TableRowColumn>Unemployed</TableRowColumn>
-					</TableRow>
-					<TableRow>
-						<TableRowColumn>3</TableRowColumn>
-						<TableRowColumn>Stephanie Sanders</TableRowColumn>
-						<TableRowColumn>Employed</TableRowColumn>
-					</TableRow>
-					<TableRow>
-						<TableRowColumn>4</TableRowColumn>
-						<TableRowColumn>Steve Brown</TableRowColumn>
-						<TableRowColumn>Employed</TableRowColumn>
-					</TableRow>
-					<TableRow>
-						<TableRowColumn>5</TableRowColumn>
-						<TableRowColumn>Christopher Nolan</TableRowColumn>
-						<TableRowColumn>Unemployed</TableRowColumn>
 					</TableRow>
 				</TableBody>
 			</Table>
