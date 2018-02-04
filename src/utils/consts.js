0aaf7f02bbebdedeab47812a166994ba7c129398
karoldavid <k.zysk@zoho.com>
2018-02-04 20:06:10 +0100

    feat: define cart item fields

---
+++ src/utils/consts.js
@@ -0,0 +1,7 @@
+export const CART_ITEM_FIELDS = [
+	{ name: "name", label: "Name", required: true },
+	{ name: "comments", label: "Comments", required: false },
+	{ name: "price", label: "Price", required: true },
+	{ name: "tax", label: "Tax", required: true },
+	{ name: "totalItehm", label: "Total Item", required: true }
+];
