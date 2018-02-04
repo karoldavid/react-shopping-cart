bd8a9c92d56034294a748a65180d6e3c72749d8a
karoldavid <k.zysk@zoho.com>
2018-02-04 21:51:48 +0100

    feat: hide checkboxes in table header and table rows

--- src/components/Table.js
+++ src/components/Table.js
@@ -8,24 +8,23 @@ import {
   TableRowColumn
 } from "material-ui/Table";
 
-/**
- * A simple table demonstrating the hierarchy of the `Table` component and its sub-components.
- */
 export default ({ header, items }) => (
   <Table>
-    <TableHeader>
+    <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
       <TableRow>
+        <TableHeaderColumn key={0}>#</TableHeaderColumn>
         {header.map((item, index) => {
           return (
-            <TableHeaderColumn key={index}>{item.label}</TableHeaderColumn>
+            <TableHeaderColumn key={index + 1}>{item.label}</TableHeaderColumn>
           );
         })}
       </TableRow>
     </TableHeader>
-    <TableBody>
-      {items.map(item => {
+    <TableBody displayRowCheckbox={false}>
+      {items.map((item, index) => {
         return (
           <TableRow>
+            <TableRowColumn key={index}>{index + 1}</TableRowColumn>
             {Object.keys(item).map((key, index) => {
               return <TableRowColumn key={index}>{item[key]}</TableRowColumn>;
             })}
