579d38b4e2831e0843c869e3fbb32a492581a19d
karoldavid <k.zysk@zoho.com>
2018-02-04 20:48:08 +0100

    refactor: import Table

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,37 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import {
-	Table,
-	TableBody,
-	TableHeader,
-	TableHeaderColumn,
-	TableRow,
-	TableRowColumn
-} from "material-ui/Table";
+import Table from "./Table";
+import { Card } from "material-ui";
 
 class ShoppingCart extends Component {
-	renderHeaderColumns = data => {
-		return data.map((item, index) => {
-			return <TableHeaderColumn key={index}>{item.label}</TableHeaderColumn>;
-		});
-	};
 
 	render() {
 		const { data } = this.props;
 
 		return (
-			<Table>
-				<TableHeader>
-					<TableRow>{this.renderHeaderColumns(data)}</TableRow>
-				</TableHeader>
-				<TableBody>
-					<TableRow>
-						<TableRowColumn>qwertz</TableRowColumn>
-						<TableRowColumn>qwertz</TableRowColumn>
-						<TableRowColumn>Employed</TableRowColumn>
-					</TableRow>
-				</TableBody>
-			</Table>
+			<Card>
+				<Table />
+			</Card>
 		);
 	}
 }
