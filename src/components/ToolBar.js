a4c9ca14538481f2a1161c29e6b3907d01d2809a
karoldavid <k.zysk@zoho.com>
2018-02-05 13:10:56 +0100

    feat: add your cart title, clear cart buttun and add new product button to toolbar

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import IconMenu from 'material-ui/IconMenu';
 import IconButton from 'material-ui/IconButton';
 import FontIcon from 'material-ui/FontIcon';
@@ -8,48 +8,21 @@ import DropDownMenu from 'material-ui/DropDownMenu';
 import RaisedButton from 'material-ui/RaisedButton';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 
-export default class ToolBar extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 3,
-    };
-  }
-
-  handleChange = (event, index, value) => this.setState({value});
+class ToolBar extends Component {
 
   render() {
     return (
       <Toolbar>
-        <ToolbarGroup firstChild={true}>
-          <DropDownMenu value={this.state.value} onChange={this.handleChange}>
-            <MenuItem value={1} primaryText="All Broadcasts" />
-            <MenuItem value={2} primaryText="All Voice" />
-            <MenuItem value={3} primaryText="All Text" />
-            <MenuItem value={4} primaryText="Complete Voice" />
-            <MenuItem value={5} primaryText="Complete Text" />
-            <MenuItem value={6} primaryText="Active Voice" />
-            <MenuItem value={7} primaryText="Active Text" />
-          </DropDownMenu>
+        <ToolbarGroup>
+          <ToolbarTitle text="Your Cart" />
         </ToolbarGroup>
         <ToolbarGroup>
-          <ToolbarTitle text="Options" />
-          <FontIcon className="muidocs-icon-custom-sort" />
-          <ToolbarSeparator />
-          <RaisedButton label="Create Broadcast" primary={true} />
-          <IconMenu
-            iconButtonElement={
-              <IconButton touch={true}>
-                <NavigationExpandMoreIcon />
-              </IconButton>
-            }
-          >
-            <MenuItem primaryText="Download" />
-            <MenuItem primaryText="More Info" />
-          </IconMenu>
+          <RaisedButton secondary label="Clear Cart" />
+          <RaisedButton primary label="Add New Product" />
         </ToolbarGroup>
       </Toolbar>
     );
   }
 }
+
+export default ToolBar
