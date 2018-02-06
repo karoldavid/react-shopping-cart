b722ed07b9586d18a6f6613f10d33a3258471b7b
karoldavid <k.zysk@zoho.com>
2018-02-06 10:11:08 +0100

    feat: validate product form input range and length

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,34 +2,41 @@ export const validate = (values, props) => {
 	const errors = {};
 
 	const requiredFields = props.fields.reduce((previous, field) => {
-		if (field.required) previous.push(field.name);
+		if (field.required) {
+			return [ ...previous, field.name];
+		}
 		return previous;
 	}, []);
 
-	requiredFields.forEach((field, index) => {
-		if (!values[field]) {
-			errors[field] = `${props.fields[index].label} is required`;
+//	console.log(props.fields)
+
+	requiredFields.forEach((name) => {
+		const index = props.fields.findIndex(field => field.name === name);
+		if (!values[name]) {
+			errors[name] = `${props.fields[index].label} is required`;
 		}
 	});
 
-	// if (
-	// 	values.firstName &&
-	// 	!containsOnlySmallAndCapitalLetters(values.firstName)
-	// ) {
-	// 	errors.firstName =
-	// 		"should contain only small and capital letters, no numbers, special characters, etc.";
-	// }
-	// if (
-	// 	values.lastName &&
-	// 	!containsOnlySmallAndCapitalLetters(values.lastName)
-	// ) {
-	// 	errors.lastName =
-	// 		"should contain only small and capital letters, no numbers, special characters, etc.";
-	// }
+	if (values.name && (values.name.length < 3 || values.name > 32)) {
+		errors.name =
+			"Name cannot be shorter than three characters or longer than 32";
+	}
 
-	// if (values.email && isEmail(values.email)) {
-	// 	errors.email = "Value should be a valid email address";
-	// }
+	if (
+		values.comments &&
+		(values.comments.length < 10 || values.comments > 32)
+	) {
+		errors.comments =
+			"Comments cannot be shorter than ten characters or longer than 32";
+	}
+
+	if (values.price && (values.price < 0 || values.price > 1000000)) {
+		errors.price = "pRICE must be a value between 0 and 1000000";
+	}
+
+	if (values.tax && (values.tax < 0 || values.tax > 100)) {
+		errors.tax = "Tax must be a value between 0 and 100";
+	}
 
 	return errors;
 };
