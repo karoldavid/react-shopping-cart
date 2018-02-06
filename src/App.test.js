682930b529d0e62b8b7afdcb974111eb7632402b
karoldavid <k.zysk@zoho.com>
2018-02-06 16:53:17 +0100

    chore: add TotalTable component tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Enzyme from "enzyme";
+import { expect } from "chai";
 import Adapter from "enzyme-adapter-react-16";
 import configureStore from "redux-mock-store";
-import renderer from 'react-test-renderer';
+import renderer from "react-test-renderer";
+
+import { RaisedButton, Table, TableRow, TableRowColumn } from "material-ui";
+
 import App from "./App";
 import ShoppingCart from "./components/ShoppingCart";
 import ToolBar from "./components/ToolBar";
+import TotalTable from "./components/TotalTable";
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -18,7 +23,7 @@ describe("App component", () => {
 	});
 
 	it("renders", () => {
-		expect(wrapper.length).toEqual(1);
+		expect(wrapper.length).to.equal(1);
 	});
 });
 
@@ -37,10 +42,9 @@ describe("ShoppingCart component", () => {
 	});
 
 	it("renders", () => {
-		expect(container.length).toEqual(1);
+		expect(container.length).to.equal(1);
 	});
 });
-
 
 describe("ToolBar component", () => {
 	const initialState = null;
@@ -54,6 +58,30 @@ describe("ToolBar component", () => {
 	});
 
 	it("renders", () => {
-		expect(container.length).toEqual(1);
+		expect(container.length).to.equal(1);
+	});
+});
+
+describe("TotalTable component", () => {
+	const taxes = {
+		netTotal: 47.25,
+		taxTotal: 5.5874999999999995,
+		taxCols: { "7": 1.96, "19": 3.61 },
+		grandTotal: 52.837500000000006
+	};
+	let totalTable;
+
+	beforeEach(() => {
+		totalTable = Enzyme.shallow(<TotalTable data={taxes} />);
+	});
+
+	it("should have one table", () => {
+		const table = totalTable.find(Table);
+		expect(table.length).to.equal(1);
+	});
+
+	it("should contain five table rows", () => {
+		const rows = totalTable.find(TableRow);
+		expect(rows.length).to.equal(5);
 	});
 });
