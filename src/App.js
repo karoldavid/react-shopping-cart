8612f4093fbb81449eca28eb62bd692628fdf9de
karoldavid <k.zysk@zoho.com>
2018-02-06 10:52:45 +0100

    refactor: wrap shopping cart commpnent in app in provider and muithemeprovider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
 import ShoppingCart from "./components/ShoppingCart";
+import { Provider } from "react-redux";
+import store from "./store";
+import { MuiThemeProvider } from "material-ui";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ShoppingCart />
+        <Provider store={store}>
+          <MuiThemeProvider>
+            <ShoppingCart />
+          </MuiThemeProvider>
+        </Provider>,
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Provider } from "react-redux";
-import store from "./store";
 import App from "./App";
-import { MuiThemeProvider } from "material-ui";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-	<Provider store={store}>
-		<MuiThemeProvider>
+
 			<App />
-		</MuiThemeProvider>
-	</Provider>,
+
 	document.getElementById("root")
 );
 registerServiceWorker();
